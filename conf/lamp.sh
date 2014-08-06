b369243bb11956db913f5a295e245c8a235243f4
Matej Kramny <git@matej.me>
2014-08-06 22:34:53 +0100

    Update lamp.sh

--- conf/lamp.sh
+++ conf/lamp.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if ![ -z "$MANDRILL_PWD" ]; then
+  echo "[smtp.mandrillapp.com]:587 $MANDRILL_PWD" > /etc/postfix/sasl_passwd
+  chown -R postfix:postfix /etc/postfix
+  chmod 600 /etc/postfix/sasl_passwd
+  postmap /etc/postfix/sasl_passwd
+fi
+
 /etc/init.d/rsyslog start
 /etc/init.d/syslog-ng start
 /usr/sbin/postfix -c /etc/postfix start
