b6ca193671299648eb856f572b1b19e95e3d2514
Matej Kramny <git@matej.me>
2014-08-06 22:43:46 +0100

    Update lamp.sh

--- conf/lamp.sh
+++ conf/lamp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if ![ -z "$MANDRILL_PWD" ]; then
+  echo "Using Mandrill Password. <= Written to /etc/postfix/sasl_passwd"
+  
   echo "[smtp.mandrillapp.com]:587 $MANDRILL_PWD" > /etc/postfix/sasl_passwd
   chown -R postfix:postfix /etc/postfix
   chmod 600 /etc/postfix/sasl_passwd
